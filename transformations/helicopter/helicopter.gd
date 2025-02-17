00c983194675d8de90dccbb9ce65509c9c882fa2
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2025-02-17 17:45:09 -0500

    Water

--- transformations/helicopter/helicopter.gd
+++ transformations/helicopter/helicopter.gd
@@ -2,13 +2,13 @@ extends RigidBody2D
 
 
 @export var speed := 4000
-@export var rotate_speed := 5000
+#@export var rotate_speed := 5000
 
-@onready var body: RigidBody2D = %Body
-@onready var pin: PinJoint2D = $PinJoint2D
+#@onready var body: RigidBody2D = %Body
+#@onready var pin: PinJoint2D = $PinJoint2D
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	apply_force(Input.get_vector(&"left", &"right", &"up", &"down") * speed)
 	#apply_force(body.global_position.direction_to(global_position) * Input.get_axis(&"down", &"up") * speed)
 	#body.apply_force(body.transform.x * Input.get_axis(&"left", &"right") * rotate_speed)
--- transformations/submarine/submarine.gd
+++ transformations/submarine/submarine.gd
@@ -6,6 +6,9 @@ extends RigidBody2D
 @onready var water_detector: Area2D = $WaterDetector
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if water_detector.get_overlapping_areas().size() > 0:
+		gravity_scale = 0.0
 		apply_force(Input.get_vector(&"left", &"right", &"up", &"down") * speed)
+	else:
+		gravity_scale = 1.0
